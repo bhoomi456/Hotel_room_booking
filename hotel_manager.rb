342b25511c227639950c89ce2ec143befb5feb31
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-01 13:32:50 +0530

    cancel booking and calculate method added

--- hotel_manager.rb
+++ hotel_manager.rb
@@ -1,4 +1,16 @@
 module HotelManager
+  
+  def add_booking(guest, room, no_of_days)
+    booking = {
+      room_number: room.room_number,
+      room_type: room.room_type,
+      price_per_day: room.price_per_day,
+      no_of_days: no_of_days
+    }
+    guest.booked_rooms << booking
+    room.available = false
+    puts "#{room.room_number} Booked successfully"
+  end
 
   def book_room(guest, room_type, no_of_days)
     room =  @rooms.find do |b|
@@ -18,16 +30,28 @@ module HotelManager
     end
   end
 
-  def add_booking(guest, room, no_of_days)
-    booking = {
-      room_number: room.room_number,
-      room_type: room.room_type,
-      price_per_day: room.price_per_day,
-      no_of_days: no_of_days
-    }
-    guest.booked_rooms << booking
-    room.available = false
-    puts "#{room.room_number} Booked successfully"
+  def calculate_bill(guest)
+    price = guest.booked_rooms[:price_per_day]
+    no_of_days = guest.booked_rooms[:no_of_days].to_i
+    price * no_of_days
   end
+
+  def find_booking(guest, room_number)
+    guest.booked_rooms.find{ |h| h[:room_number] == room_number}
+  end
+
+  def cancel_booking(guest, room_number)
+    if find_booking(guest, room_number) != nil
+      room = find_room(room_number)
+      guest.booked_rooms.reject! {|booking| booking[:room_number] == room_number}  
+      room.available = true
+      puts "#{room_number} Cancelled successfully"
+    else
+      puts "#{room_number} has no booking with #{guest.guest_id}"
+    end
+  end
+
+  
+
 end
 
