dd821c43d9958f8c67c9530483378652bb2e5a78
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-01 13:07:54 +0530

    modules removed for later testing

--- hotel_manager.rb
+++ hotel_manager.rb
@@ -1,7 +1,3 @@
-require_relative "hotel"
-require_relative  "guest"
-require_relative "room"
-
 module HotelManager
 
   def book_room(guest, room_type, no_of_days)
@@ -10,7 +6,7 @@ module HotelManager
     end
 
     guest = @guests.find {|g| g.guest_id == guest.guest_id}
-    
+
     if guest == nil
       raise GuestNotFound, "Guest not found"
     elsif room == nil
@@ -30,20 +26,8 @@ module HotelManager
       no_of_days: no_of_days
     }
     guest.booked_rooms << booking
-    puts "#{romm.room_number} Booked successfully"
+    room.available = false
+    puts "#{room.room_number} Booked successfully"
   end
 end
 
-r = Room.new(1, "regular", 2000)
-r2 = Room.new(2, "regular", 2000)
-
-g = Guest.new("bhoomi", 101)
-g2 = Guest.new("bhoomi", 102)
-
-h = Hotel.new("taj")
-
-h.add_rooms(r)
-h.add_rooms(r2)
-h.add_guests(g)
-h.add_guests(g2)
-h.book_room(g, "regular", 2)
