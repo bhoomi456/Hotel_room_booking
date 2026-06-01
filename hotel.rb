4605d8b55778a689d5079bb4abdd5a1a06506f5f
Bhoomi jain <bhoomij0304@gmail.com>
2026-06-01 12:07:42 +0530

    Added find _room and find_guest methods (#10)
    * Added find_rooms method and validating rooms existance
    
    * Added find guest and validating guest existance

--- hotel.rb
+++ hotel.rb
@@ -7,13 +7,29 @@ class Hotel
     @guests = []
   end
 
+  def find_room(room)
+    @rooms.find {|r| r.room_number == room.room_number}
+  end
+
+  def find_guests(guest)
+    @guests.find {|g| g.guest_id == guest.guest_id}
+  end
+
   def add_rooms(room)
-    @rooms << room
-    puts "Room added successfully"
+    if find_room(room) != nil
+      @rooms << room
+      puts "Room added successfully"
+    else
+      puts "#{room.room_number} Room already exist"
+    end
   end
 
   def add_guests(guest)
-    @guests << guest
-    puts "#{guest.name} guest added successfully"
+    if find_guests(guest) != nil
+      @guests << guest
+      puts "#{guest.name} guest added successfully"
+    else
+      puts "#{guest.guest_id} Already registered"
+    end
   end
 end
