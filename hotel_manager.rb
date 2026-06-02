5e432174b08b4be821d08f7de1efbf8ae83fba8d
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-02 13:38:27 +0530

    Remove add booking method

--- hotel_manager.rb
+++ hotel_manager.rb
@@ -1,17 +1,5 @@
 module HotelManager
   
-  def add_booking(guest, room, no_of_days)
-    booking = {
-      room_number: room.room_number,
-      room_type: room.room_type,
-      price_per_day: room.price_per_day,
-      no_of_days: no_of_days
-    }
-    guest.booked_rooms << booking
-    room.available = false
-    puts "#{room.room_number} Booked successfully"
-  end
-
   def book_room(guest_id, room_type, no_of_days)
     room =  @rooms.find do |b|
       b.room_type == room_type && b.available == true
