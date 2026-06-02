5f12b9d71c796d1d6cab27216a507798fb8f8301
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-02 13:32:56 +0530

    Add class variable for auto generated room number

--- room.rb
+++ room.rb
@@ -1,9 +1,11 @@
 class Room 
+  @@room_number = 0
 
   attr_accessor :room_number, :room_type, :price_per_day, :available
 
-  def initialize(room_number, room_type, price_per_day)
-    @room_number = room_number
+  def initialize(room_type, price_per_day)
+    @@room_number += 1
+    @room_number = @@room_number
     @room_type = room_type
     @price_per_day = price_per_day
     @available = true
