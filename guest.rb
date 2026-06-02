824f17d5c84802e64a53246d93737359b85ecfcc
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-02 13:30:03 +0530

    add class variable for auto generated guest id

--- guest.rb
+++ guest.rb
@@ -1,9 +1,11 @@
 class Guest 
+  @@guest_id = 0
 
-  attr_accessor :name, :guest_id, :booked_rooms
-  def initialize(name, guest_id)
+  attr_accessor :name, :guest_id
+  
+  def initialize(name)
+    @@guest_id += 1
     @name = name
-    @guest_id = guest_id
-    @booked_rooms = []
+    @guest_id = @@guest_id
   end
 end
