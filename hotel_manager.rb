3e0a40f0aa8090321ffffc7dea8b67a2e77f87d4
Bhoomi jain <bhoomij0304@gmail.com>
2026-06-02 14:01:46 +0530

    Hotel manager (#35)
    * Remove add booking method
    
    * Refactor book room method
    
    * rRefactor calculated bill method
    
    * Remove find booking method
    
    * Refactor cancel booking method
    
    * Refactor display booking details method
    
    * Added checkout feature
    
    * Added dispaly all booking details
    
    ---------
    
    Co-authored-by: Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>

--- hotel_manager.rb
+++ hotel_manager.rb
@@ -1,23 +1,9 @@
 module HotelManager
   
-  def add_booking(guest, room, no_of_days)
-    booking = {
-      room_number: room.room_number,
-      room_type: room.room_type,
-      price_per_day: room.price_per_day,
-      no_of_days: no_of_days
-    }
-    guest.booked_rooms << booking
-    room.available = false
-    puts "#{room.room_number} Booked successfully"
-  end
-
-  def book_room(guest_id, room_type, no_of_days)
+  def book_room(guest, room_type, no_of_days)
     room =  @rooms.find do |b|
-      b.room_type == room_type && b.available == true
+      b.room_type == room_type && b.available
     end
-
-    guest = @guests.find {|g| g.guest_id == guest_id}
 
     if guest == nil
       raise GuestNotFound, "Guest not found"
@@ -26,48 +12,83 @@ module HotelManager
     elsif no_of_days <= 0
       raise InvalidNumberOfDays, "Invalid number of days Entered"
     else
-      add_booking(guest, room, no_of_days)
+      booking = Booking.new(guest, room, no_of_days)
+      @booked_rooms << booking
+      room.available = false
+      puts "Room booked successfully"
     end
   end
 
   def calculate_bill(guest)
     total_amount = 0
-    guest.booked_rooms.each do |booking|
-      price = booking[:price_per_day].to_f
-      no_of_days = booking[:no_of_days].to_i
-      total_amount += no_of_days * price
+    @booked_rooms.each do |b|
+      if b.guest.guest_id == guest.guest_id && b.status == "closed"
+        price_per_day = b.room.price_per_day
+        no_of_days = b.no_of_days
+        total_amount += price_per_day * no_of_days
+      end
     end
-    return total_amount
+    total_amount
   end
 
-  def find_booking(guest, room_number)
-    guest.booked_rooms.find{ |h| h[:room_number] == room_number}
-  end
-
-  def cancel_booking(guest, room)
-    if find_booking(guest, room.room_number) != nil
-      guest.booked_rooms.reject! {|booking| booking[:room_number] == room.room_number}  
-      room.available = true
-      puts "#{room.room_number} Cancelled successfully by #{guest.name}"
-      # puts guest.booked_rooms.length
+  def cancel_booking(guest)
+    if @booked_rooms.find {|b| b.guest.guest_id == guest.guest_id && b.status == "active"} != nil
+      @booked_rooms.each do |b|
+        if b.guest.guest_id == guest.guest_id
+          b.status = "cancelled"
+          b.room.available = true
+        end
+      end
+      puts "Booking cancelled successfully"
     else
-      puts "Room number : #{room.room_number} has no booking with #{guest.name}-#{guest.guest_id}"
+      puts "No boooking found"
     end
   end
 
   def display_booking_details(guest)
-    if guest.booked_rooms.length >= 1
-      puts "----Booking Details----"
-      guest.booked_rooms.each do |b|
-        puts "Guest Name : #{guest.name}"
-        puts "Room number : #{b[:room_number]}"
-        puts "Room Type : #{b[:room_type]}"
-        puts "Price per day : #{b[:price_per_day]}"
-        puts "No of days : #{b[:no_of_days]}"
-        puts "Total amount : #{calculate_bill(guest)}"
-      end
-    else
+    guest_booked_room = @booked_rooms.select {|b| b.guest.guest_id == guest.guest_id}
+
+    if guest_booked_room.empty?
       puts "There is no Booking!"
+    else
+      puts "\n"
+      puts "----Booking Details----"
+      guest_booked_room.each do |b|
+        puts "Guest Name : #{guest.name}"
+        puts "Room number : #{b.room.room_number}"
+        puts "Room Type : #{b.room.room_type}"
+        puts "Price per day : #{b.room.price_per_day}"
+        puts "No of days : #{b.no_of_days}"
+        puts "Status : #{b.status}"
+        puts "\n"
+      end
+    end
+  end
+
+  def checkout(guest)
+    guest_booked_room = @booked_rooms.select {|b| b.guest.guest_id == guest.guest_id && b.status == "active"}
+    if guest_booked_room.empty?
+      puts "No booking found"
+    else
+      guest_booked_room.each do |b|
+        b.status = "closed"
+        b.room.available = true
+      end
+      puts "Checked out successfully"
+      puts "Total amount : #{calculate_bill(guest)}"
+    end
+  end
+
+  def display_all_booking_details
+    puts "----All Booking Details----"
+    @booked_rooms.each do |b|
+      puts "Guest Name : #{b.guest.name}"
+      puts "Room number : #{b.room.room_number}"
+      puts "Room Type : #{b.room.room_type}"
+      puts "Price per day : #{b.room.price_per_day}"
+      puts "No of days : #{b.no_of_days}"
+      puts "Status : #{b.status}"
+      puts "\n"
     end
   end
 end
